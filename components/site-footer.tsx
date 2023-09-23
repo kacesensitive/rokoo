26a92c92dfffdbe834984835f84af839c9ab3611
kacesensitive <kc@kacey.dev>
2023-09-23 14:15:08 -0500

    fix builds

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,16 +1,9 @@
-import { siteConfig } from "@/config/site";
 import Link from "next/link";
 
 export default function Footer() {
     return (
         <footer className="w-full text-center p-4 mt-4 bg-background text-foreground">
-            <Link href="/privacy">
-                <span className="ml-4">Privacy Policy</span>
-            </Link>
-            <Link href="/terms">
-                <span className="ml-4">Terms of Service</span>
-            </Link>
-            <p className="text-sm mt-2">© {new Date().getFullYear()} {siteConfig.name}</p>
+            <p>Made with ❤️ by <Link href={"kacey.dev"} className="text-red-600">kacey.dev</Link></p>
         </footer>
     )
 }
